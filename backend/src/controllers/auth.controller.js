3657d0751bc02f534a402d6f1fbd8adf93e68278
Unknown01987 <ydwivedi494@gmail.com>
2025-03-29 23:07:28 +0530

    corrected  test

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -74,6 +74,16 @@ export const login =  async (req,res)=>{
     }
 };
 
+export const logout =  (req,res)=>{
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    }
+    catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
 
 export const updateProfile = async (req, res) => {
     try {
