0e453a5b0570ab682a01526611ab7700717bd5a5
Unknown01987 <ydwivedi494@gmail.com>
2025-03-31 19:54:33 +0530

    Signup Login and Logout  frontend completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
@@ -15,14 +17,51 @@ export const useAuthStore = create((set, get) => ({
           const res = await axiosInstance.get("/auth/check");
     
           set({ authUser: res.data });
-          get().connectSocket();
+          //get().disconnectSocket();
         } catch (error) {
           console.log("Error in checkAuth:", error);
           set({ authUser: null });
         } finally {
           set({ isCheckingAuth: false });
         }
-      },
-    
+    },
+
+    signup: async (data) => {
+      set({ isSigningUp: true });
+      try {
+        const res = await axiosInstance.post("/auth/signup", data);
+        set({ authUser: res.data });
+        toast.success("Account created successfully");
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isSigningUp: false });
+      }
+    },
+
+    logout: async () => {
+      try {
+        await axiosInstance.post("/auth/logout");
+        set({ authUser: null });
+        toast.success("Logged out successfully");
+        //get().disconnectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      }
+    },
+
+    login: async (data) => {
+      set({ isLoggingIn: true });
+      try {
+        const res = await axiosInstance.post("/auth/login", data);
+        set({ authUser: res.data });
+        toast.success("Logged in successfully");
+        //get().connectSocket();
+      } catch (error) {
+        toast.error(error.response.data.message);
+      } finally {
+        set({ isLoggingIn: false });
+      }
+    },
 
 }));
