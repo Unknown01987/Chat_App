0aeca7e2cf163bc883fda796d419f41447ad9596
Unknown01987 <ydwivedi494@gmail.com>
2025-04-07 21:15:05 +0530

    check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,6 @@ import ProfilePage from "./pages/ProfilePage";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { useThemeStore } from "./store/useThemeStore";
-import { Loader } from "lucide-react";
-
-import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
